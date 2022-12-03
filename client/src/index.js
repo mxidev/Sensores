e7bfae3856171251266689a7ad94ac5e834cdfcb
maximiliano.tapia <you@example.com>
2022-12-02 21:22:13 -0300

    Update datos y grafico Radon

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,4 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Router>
-    <App />
-  </Router>
-)
+root.render(<Router> <App /> </Router>)
--- client/src/pages/SensorData/Radon.js
+++ client/src/pages/SensorData/Radon.js
@@ -1,46 +1,76 @@
 import React from 'react';
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Chart } from 'react-google-charts';
 
 const Radon = () => {
 
     const [datos, setDatos] = useState([]);
+    const [fechas, setFechas] = useState([]);
+    var radonData = new Array();
+    var radonFecha = new Array();
+    var arrayData = new Array();
+    var arrayFecha = new Array();
 
-    const loadSensor = () => {
-        fetch('http://localhost:5005/api/v1/radon')
-            .then(res => res.json())
-            .then(allData => setDatos(allData))
+    useEffect(() => {
+        axios.get('http://localhost:5005/api/v1/radon')
+            .then(res => {
+                const data = res.data;
+                setDatos(data.Radon)
+            })
+    }, [])
+
+    useEffect(() => {
+        axios.get('http://localhost:5005/api/v1/radon')
+            .then(res => {
+                const data = res.data;
+                setFechas(data.Fecha)
+            })
+    }, [])
+
+    radonData = datos // Array -> [{...},{ts:...,value:...},{...}, ... ,{...},{...}]
+    radonFecha = fechas // Array -> [{...},{ts:...,value:...},{...}, ... ,{...},{...}]
+
+    for(var i in datos){
+        arrayData.unshift(parseInt(Object.values(radonData[i])[1],10)); // Array -> [value, value, ...]
     }
-
-    loadSensor()
-
-    /*
-    const datos = {
-        Fecha: [
-            {},{},{},...,{},{}
-        ],
-        Radon: [
-            {},{},{},...,{},{}
-        ]
-        Key: [
-            {Value}, {Value}, ... , {Value}, {Value}
-        ]
+    for(var i in fechas){
+        arrayFecha.unshift(Object.values(radonFecha[i])[1]); // Array -> [value, value, ...]
     }
-     */
+    console.log(arrayData)
+    console.log(arrayFecha)
+
+    var data = [
+        ["Fecha", "Data"],
+        ["2004", 1000],
+        ["2005", 1170],
+        ["2006", 660],
+        ["2007", 460],
+        ["2008", 400],
+        ["2009", 1120],
+        ["2010", 540]
+        // esta es la idea --> [fecha, data]
+    ]
+    var options = {
+        title: 'Radon Data',
+        curveType: 'function',
+        legend: {
+            position: "bottom"
+        }
+    }
 
     return (
         <main>
-            <h1>Aqui van los datos Radon</h1>
+            <h1>Sensor: Radon</h1>
             <hr />
-            {Object.keys(datos).map((key)=>{
-                return(
-                    <div key={key}>
-                        <h5>
-                            {key}:
-                        </h5>
-                    </div>
-                )
-            })}
+            <Chart
+                chartType="LineChart"
+                width="100%"
+                height="500px"
+                data={data}
+                options={options}
+            />
             <Link to="/">Volver</Link>
         </main>
     )
